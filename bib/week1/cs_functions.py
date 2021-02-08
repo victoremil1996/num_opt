da4e953489a790b10dc7b7ac2916745ecf8d65f7
LJBCPH <lucasjohanboesen123@gmail.com>
2021-02-08 18:18:02 +0100

    Lavet F1

--- bib/week1/cs_functions.py
+++ bib/week1/cs_functions.py
@@ -3,17 +3,16 @@ Contains functions for numerical optimisation analysis
 """
 
 from typing import Union
-
 import numpy as np
 from numpy import ndarray
 
-def HelloWorld():
-    print("Hello World")
+result = 0
+def f1(x: ndarray, a: ndarray = 1000, result=0):
+    for i in range(x.shape[0]):
+        result += np.multiply(x[i]**2, a**(i / (x.shape[0]-1)))
 
-def f1(x,d):
-    return x+d
+    return result
 
-f1(1,10)
 
 def f2(x):
     return x

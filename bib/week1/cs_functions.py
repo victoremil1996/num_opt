f0ef7a49ce502780233224d7250acd66f32dd94d
LJBCPH <lucasjohanboesen123@gmail.com>
2021-02-08 18:21:38 +0100

    Lavet F1

--- bib/week1/cs_functions.py
+++ bib/week1/cs_functions.py
@@ -6,7 +6,7 @@ from typing import Union
 import numpy as np
 from numpy import ndarray
 
-result = 0
+
 def f1(x: ndarray, a: ndarray = 1000, result=0):
     for i in range(x.shape[0]):
         result += np.multiply(x[i]**2, a**(i / (x.shape[0]-1)))

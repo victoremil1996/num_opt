06c5c9144c693925d48ce7e1ff660fcae6abdf98
LJBCPH <lucasjohanboesen123@gmail.com>
2021-02-08 18:38:16 +0100

    Lavet F3 og h fkt.

--- bib/week1/cs_functions.py
+++ bib/week1/cs_functions.py
@@ -5,8 +5,9 @@ Contains functions for numerical optimisation analysis
 from typing import Union
 import numpy as np
 from numpy import ndarray
+import math
 
-result = 0
+
 def f1(x: ndarray, a: ndarray = 1000, result=0):
     for i in range(x.shape[0]):
         result += np.multiply(x[i]**2, a**(i / (x.shape[0]-1)))
@@ -14,5 +15,11 @@ def f1(x: ndarray, a: ndarray = 1000, result=0):
     return result
 
 
-def f2(x):
-    return x
+def f3(x: ndarray, epsilon=10**(-16)):
+
+    return math.log(epsilon+f1(x))
+
+
+def fh(x, q=10**8):
+
+    return math.log(1+math.exp(q*x)) / q

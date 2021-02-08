6e46f7f3f822651a56e54851934bbb2c912da436
VictorEmil-hub <Victoremilskovlundmark@gmail.com>
2021-02-08 17:31:33 +0100

    Oprettet bib og tilføjet week1 pakke

---
+++ bib/week1/cs_functions.py
@@ -0,0 +1,12 @@
+"""
+Contains functions for numerical optimisation analysis
+"""
+
+from typing import Union
+
+import numpy as np
+from numpy import ndarray
+
+def HelloWorld():
+    print("Hello World")
+

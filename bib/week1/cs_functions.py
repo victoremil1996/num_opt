ee64f53aedd785613d9132cf9182d0205674ee9d
LJBCPH <lucasjohanboesen123@gmail.com>
2021-02-11 00:06:09 +0100

    Taylor f_1 og grad, hesse

--- bib/week1/cs_functions.py
+++ bib/week1/cs_functions.py
@@ -13,15 +13,15 @@ def f1(x: ndarray, a: ndarray = 1000):
     for i in range(x.shape[0]):
         fv += x[i]**2 * a**(i / (x.shape[0]-1))
 
-    grad = 0
+    grad = []
     for i in range(x.shape[0]):
-        grad += 2 * x[i] * a ** (i / (x.shape[0] - 1))
+        grad.append(2 * x[i] * a ** (i / (x.shape[0] - 1)))
 
-    hess = 0
+    hess = []
     for i in range(x.shape[0]):
-        hess += 2 * a ** (i / (x.shape[0] - 1))
+        hess.append(2 * a ** (i / (x.shape[0] - 1)))
 
-    return fv, grad, hess
+    return fv, grad, np.diagflat(np.diag(np.full((x.shape[0], x.shape[0]), hess)))
 
 
 def f2(x: ndarray):
@@ -30,17 +30,16 @@ def f2(x: ndarray):
     else:
         fv = (1-x[0])**2 + 100*(x[1]-x[0]**2)**2
         grad = np.array([-2*(1-x[0])-400*x[0]*(x[1]-x[0]**2), 200*(x[1]-x[0]**2)])
-        hess = np.array([[-400*(x[1]-x[0])+800*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
+        hess = np.array([[-400*(x[1]-x[0]**2)+800*x[0]**2+2, -400*x[0]], [-400*x[0], 200]])
 
     return fv, grad, hess
 
 
-def f3(x: ndarray, epsilon=10**(-16), a=2):
-
-    fv = np.log(epsilon+f1(x, a)[0])
-    grad = f1(x, a)[1]/(epsilon+f1(x, a)[0])
-
-    return fv, grad
+def f3(x: ndarray, eps=10**(-16), a: ndarray = 2):
+    fv = np.log(eps+f1(x, a)[0])
+    grad = f1(x, a)[1]/(eps+f1(x, a)[0])
+    hess = (f1(x, a)[2]*(eps+f1(x, a)[0])-np.multiply(f1(x, a)[1], f1(x, a)[1]))/(f1[x,a]**2+eps)
+    return fv, grad, hess
 
 
 def fh(x: ndarray, q=10**8):
@@ -55,5 +54,19 @@ def f4(x: ndarray, q=10**8, result=0):
 
 def f5(x: ndarray, q=10**8, result=0):
     for i in range(x.shape[0]):
-        result += fh(x[i])**2+100*fh(-x[i])**2
+        result += np.power(fh(x[i]), 2)+100*np.power(fh(-x[i]), 2)
     return result
+
+
+def A44(a, eps, x: ndarray):
+    out = []
+    for i in range(x.shape[0]):
+        out.append(((x[i]+eps)**2 * a**(i / (x.shape[0]-1))-((x[i])**2 * a**(i / (x.shape[0]-1)))) / eps)
+    return out
+
+
+def A45(a, eps, x: ndarray):
+    out = []
+    for i in range(x.shape[0]):
+        out.append((2 * (x[i]+eps) * a ** (i / (x.shape[0] - 1))-(2 * x[i] * a ** (i / (x.shape[0] - 1)))) / eps)
+    return out
--- bib/week1/plotting.py
+++ bib/week1/plotting.py
@@ -6,46 +6,78 @@ import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 import numpy as np
 from numpy import ndarray
-from bib.week1.cs_functions import f1, f2, f3, fh,f4,f5
+from bib.week1.cs_functions import f1, f2, f3, fh, f4, f5, A44
 
-# Data for a three-dimensional line
-# zline = np.array(np.linspace(0, 15, 1000))
-xline = np.array(range(-1, 4), dtype='int64')
-yline = np.array(range(-1, 4), dtype='int64')
 xline = np.array(np.linspace(-2.5, 4, 55))
 yline = np.array(np.linspace(-2.5, 4, 55))
+zline = np.array(np.linspace(-2.5, 4, 55))
 X, Y = np.meshgrid(xline, yline)
 xy = np.array([X, Y])
+
+# Alternative method to populate functions:
+Res = np.empty((xline.shape[0], yline.shape[0]))
+for i, x in enumerate(xline):
+    for j, y in enumerate(yline):
+        Res[i, j] = f4(np.array([x, y]))
+
+# Plots
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 # f1
-# ax.plot3D(yline, xline, f1(xy[0:], np.array(100))[0], 'grey')
-# ax.contour3D(X, Y, f1(xy[0:], np.array(1))[0:2][0], 50, cmap='binary')
 ax.plot_surface(X, Y, f1(xy[0:], np.array(2))[0], rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
 ax.set_title('Ellipsoid')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('f_1 value')
+
 # f2
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot_surface(X, Y, f2(xy[0:])[0], rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
 ax.set_title('Banana')
-
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('f_2 value')
 
 # f3
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot_surface(X, Y, f3(xy[0:], 2)[0], rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
-
+ax.set_title('Log-Ellipsoid')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('f_3 value')
 
 # f4
 fig = plt.figure()
 ax = plt.axes(projection='3d')
 ax.plot_surface(X, Y, f4(xy), rstride=1, cstride=1,
                 cmap='viridis', edgecolor='none')
+ax.set_title('Attractive Sector Function 1')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('f_4 value')
 
 # xy.shape
 # f3(xy[0:],2)[0:2][0].shape
+fig = plt.figure()
+ax = plt.axes(projection='3d')
+ax.plot_surface(X, Y, f5(xy), rstride=1, cstride=1,
+                cmap='viridis', edgecolor='none')
+ax.set_title('Attractive Sector Function 2')
+ax.set_xlabel('X')
+ax.set_ylabel('Y')
+ax.set_zlabel('f_5 value')
 
-# xy[0:][1].shape
+# TESTS
+# Eksempel på test af gradient for f_1
+A44(1000, 10**(-8), np.array([0,-1.3, 987,-323, 2.32]))
+f1(np.array([0,-1.3, 987,-323, 2.32]))[2]
+np.multiply(f1(np.array([0,-1.3, 987,-323, 2.32]))[1],(np.array([0,-1.3, 987,-323, 2.32])[1]))
+
+f3(np.array([1,2]),10**(-8))
+
+A45(1000, 10**(-8), np.array([0,-1.3, 987,-323, 2.32]))

cf4364b25782109473674392475784d777d8ce0b
LJBCPH <lucasjohanboesen123@gmail.com>
2021-02-18 20:32:00 +0100

    Tilføjet test miljø med plot

--- bib/week1/tests.py
+++ bib/week1/tests.py
@@ -10,7 +10,8 @@ from mpl_toolkits import mplot3d
 import numpy as np
 from numpy import ndarray
 from numpy import *
-from bib.week1.cs_functions import f1, f2, f3, fh, f4, f5, A44
+from bib.week1.cs_functions import *
+
 
 def f1ToTest(x: ndarray, a: ndarray = 1000):
     fv = 0
@@ -32,15 +33,28 @@ def f3ToTest(x: ndarray, eps=10**(-16), a: ndarray = 2):
     fv = np.log(eps+f1(x, a)[0])
     return fv
 
+
+def f1Grad(x: ndarray, a: ndarray = 1000):
+    grad = []
+    for i in range(x.shape[0]):
+        grad.append(2 * x[i] * a ** (i / (x.shape[0] - 1)))
+    return grad
+
+
 x0 = np.array([7, 3])
 history = [np.linalg.norm(f1(x0)[1])]
 def callback(x):
     fobj = np.linalg.norm(f1(x)[1])
     history.append(fobj)
 
-result = minimize(f1ToTest, x0, method='BFGS', tol=1e-8, callback=callback)
-print(history)
 
-plt.plot(np.log10(history))
+result = minimize(f2ToTest, x0, method='BFGS', tol=10**(-8), callback=callback)
+
+result_opt = minimize(f1ToTest, x0, method='BFGS', tol=10**(-8), callback=callback, options={'disp': True}, jac=f1Grad)
+print(history)
+result.x
+plt.plot((np.log(history)))
+
+
 np.log(history)
 result = minimize(f4, x0, method='BFGS', tol=1e-8)

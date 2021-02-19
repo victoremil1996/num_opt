8344eb15b7c55610c72733361d0d66b8d8961f36
LJBCPH <lucasjohanboesen123@gmail.com>
2021-02-19 01:52:13 +0100

    Test med plots

--- bib/week1/tests.py
+++ bib/week1/tests.py
@@ -14,31 +14,38 @@ from numpy import *
 from bib.week1.cs_def import *
 
 def callback(x):
-    fobj = np.linalg.norm(grad1(x))
+    fobj = np.linalg.norm(grad4(x))
     history.append(fobj)
+    history_fv.append(fv4(x))
 
 
-x0 = np.array(random.randint(-100, 100, size=50))
-history = [np.linalg.norm(grad1(x0))]
-
+x0 = np.array(random.randint(-10, 10, size=2))
+#x0 = np.array([100,2])
+history = [np.linalg.norm(grad4(x0))]
+history_fv = [fv4(x0)]
+fv4(x0)
 #BFGS
-result_BFGS = minimize(fv1, x0, method='BFGS', tol=10**(-8), callback=callback, jac=grad1)
+result_BFGS = minimize(fv4, x0, method='BFGS', tol=10**(-8), callback=callback, jac=grad4)
 #Newton-CG
-result_NM = minimize(fv1, x0, method='Newton-CG', tol=10**(-8), callback=callback,
-                     jac=grad1, hess=hess1)
+result_NM = minimize(fv4, x0, method='Newton-CG', tol=10**(-15), callback=callback, jac=grad4)
 #Trust-NCG
-result_TNCG = minimize(fv1, x0, method='trust-ncg', tol=10**(-8), callback=callback,
-                       jac=grad1, hess=hess1)
+result_TNCG = minimize(fv4, x0, method='trust-ncg', tol=10**(-8), callback=callback, jac=grad4)
+#Neal
+result_Nelder = minimize(fv3, x0, method='Nelder-Mead', tol=10**(-8), callback=callback)
 #plt.plot((np.log(history)))
 plt.semilogy(history)
+#plt.plot(history_fv)
 plt.legend(['BFGS', 'Newton-CG', 'trust-ncg'])
-plt.title('Optimization Algorithms with d=50')
-plt.ylabel('log$||f||_1$')
+plt.title('Optimization Algorithms - Attractive-Sector 1, d=1000')
+#plt.ylabel('log$||∇f||_2$')
+plt.ylabel('$f$')
 plt.xlabel('Iterations')
-
-fv1(result_BFGS.x)
-result_BFGS.x
-result_NM.x
-result_TNCG.x
-np.log(history)
-result = minimize(f4, x0, method='BFGS', tol=1e-8)
+help(result_NM)
+result_BFGS.nfev
+fv4(result_BFGS.x)
+result_NM.nfev
+fv4(result_NM.x)
+result_TNCG.maxcv
+fv4(result_TNCG.x)
+fv3(result_Nelder.x)
+grad3(result_Nelder.x)
